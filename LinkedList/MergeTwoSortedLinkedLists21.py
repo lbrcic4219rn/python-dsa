a553ca066d65ff16c64712ad0de0386473f92234
lukabrcic <lbrcic4219rn@raf.edu.rs>
2026-06-07 19:03:33 +0200

    Merge two sorted Linked lists

---
+++ LinkedList/MergeTwoSortedLinkedLists21.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from LinkedList.ListNode import ListNode
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1 is not None:
+            curr.next = list1
+        else:
+            curr.next = list2
+
+        return dummy.next
+
+
+
+
+
+
+# Input: list1 = [1,2,4], list2 = [1,3,5]
+#
+# Output: [1,1,2,3,4,5]
--- SlidingWindow/LongestSubstringWithoutRepeatingCharacters3.py
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters3.py
@@ -1,3 +1,12 @@
-# todo
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        maxLen = 0
+        seen = set()
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            maxLen = max(maxLen, r - l + 1)
+        return maxLen

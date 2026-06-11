9eea642b038f59b92aa69fc52c960589c6d7ee34
lukabrcic <lukabrcic.lb@gmail.com>
2026-06-11 22:52:44 +0200

    Solution for leet code 3: Maximum Depth of Binary Tree

---
+++ Trees/MaximumDepthOfBinaryTree3.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from Trees.TreeNode import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return max(left, right) + 1

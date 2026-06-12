0f6a7239a0f564f45cb74725b1d5e599a71ad753
lukabrcic <lukabrcic.lb@gmail.com>
2026-06-12 15:01:37 +0200

    Solution for: ReorderLinkedList143

---
+++ LinkedList/ReorderLinkedList143.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from LinkedList.ListNode import ListNode
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second and slow:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2

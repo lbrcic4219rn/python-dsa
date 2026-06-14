4e93855425c66a01bdf542cf1b6f16d8647d6109
lukabrcic <lukabrcic.lb@gmail.com>
2026-06-14 21:45:28 +0200

    Solution for: LongestRepeatingCharacterReplacement424

--- SlidingWindow/LongestRepeatingCharacterReplacement424.py
+++ SlidingWindow/LongestRepeatingCharacterReplacement424.py
@@ -1,3 +1,19 @@
-# todo
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        maxFreq = 0
+        count = {}
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            ch = s[r]
+            count[ch] = count.get(ch, 0) + 1
+            maxFreq = max(maxFreq, count[ch])
+
+            if (r - l + 1) - maxFreq > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res

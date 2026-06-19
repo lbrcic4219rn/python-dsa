172ae2ec8d380c7dd680c655cd46180f9611ebe7
lukabrcic <lukabrcic.lb@gmail.com>
2026-06-19 22:14:54 +0200

    Solution for: Jumpgame55

---
+++ Greedy/JumpGame55.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxJump = 0
+
+        for i in range(len(nums)):
+            if i >= maxJump:
+                return False
+            maxJump = max(maxJump, i + nums[i])
+
+        return True

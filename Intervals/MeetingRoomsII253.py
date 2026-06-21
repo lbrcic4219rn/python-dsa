a45bac7aa3c10f95a3d00ef144f178c32473a7a1
lukabrcic <lukabrcic.lb@gmail.com>
2026-06-21 22:26:35 +0200

    Solution for: MeetingRoomsII253

---
+++ Intervals/MeetingRoomsII253.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        starts = sorted(i.start for i in intervals)
+        ends = sorted(i.end for i in intervals)
+
+        res = count = 0
+        s = e = 0
+
+        while s < len(starts):
+            if starts[s] < ends[e]:
+                count += 1
+                s += 1
+            else:
+                count -= 1
+                e += 1
+            res = max(res, count)
+
+        return res

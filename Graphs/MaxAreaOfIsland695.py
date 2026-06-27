71c3b11069101b567532dad5f8fe596369a43f95
lukabrcic <lukabrcic.lb@gmail.com>
2026-06-27 20:01:15 +0200

    Solution for: MaxAreaOfIsland695

---
+++ Graphs/MaxAreaOfIsland695.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        maxSize = 0
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == 0:
+                return 0
+
+            grid[r][c] = 0
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] != 0:
+                    maxSize = max(maxSize, dfs(i, j))
+        return maxSize

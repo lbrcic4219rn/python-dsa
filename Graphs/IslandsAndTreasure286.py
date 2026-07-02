da712160de3b17ec39a5ae3e74309924ac49e62b
lukabrcic <lukabrcic.lb@gmail.com>
2026-07-02 18:29:28 +0200

    Solution for: IslandsAndTreasure286

---
+++ Graphs/IslandsAndTreasure286.py
@@ -0,0 +1,32 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        INF = 2147483647
+        queue = deque()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    queue.append((r, c))
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while queue:
+            r, c = queue.popleft()
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if (
+                        nr < 0 or nr >= ROWS or
+                        nc < 0 or nc >= COLS or
+                        grid[nr][nc] != INF
+                ):
+                    continue
+
+                grid[nr][nc] = grid[r][c] + 1
+                queue.append((nr, nc))

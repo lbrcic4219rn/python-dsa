6765726c1b5a9d6b0107f0c7a9fbec21a7373b6f
lukabrcic <lukabrcic.lb@gmail.com>
2026-07-13 21:30:59 +0200

    Solution for: RemoveElement27

---
+++ Arrays&Hashing/MajorityElement169.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, count = 0, 0
+
+        for num in nums:
+            if count == 0:
+                res = num
+            count += (1 if res == num else -1)
+
+        return res

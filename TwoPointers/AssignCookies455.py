f06efe7b0235bb8d0836867c00912465531ab96a
lukabrcic <lukabrcic.lb@gmail.com>
2026-07-14 13:19:57 +0200

    Solution for: AssignCookies455

---
+++ TwoPointers/AssignCookies455.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i = j = res = 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                res += 1
+                i += 1
+                j += 1
+                continue
+            j += 1
+
+        return res

6ba17fa6fe3de47950cba94069c85528942bfcd6
lukabrcic <lukabrcic.lb@gmail.com>
2026-07-15 13:24:42 +0200

    Solution for: SortAnArray912

---
+++ Arrays&Hashing/SortColors75.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l, r = 0, len(nums) - 1
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+            if nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                i -= 1
+            i += 1

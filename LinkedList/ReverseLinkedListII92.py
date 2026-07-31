cc1ff22a83eb30a72d80a671e568a0d6be2f5aac
lukabrcic <lukabrcic.lb@gmail.com>
2026-07-31 12:46:41 +0200

    Solution for: ReverseLinkedListII92

---
+++ LinkedList/ReverseLinkedListII92.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from LinkedList.ListNode import ListNode
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        leftPrev, cur = dummy, head
+
+        for _ in range(left - 1):
+            leftPrev, cur = cur, cur.next
+
+        prev = None
+        for _ in range(right - left + 1):
+            tmpNext = cur.next
+            cur.next = prev
+            prev, cur = cur, tmpNext
+
+        leftPrev.next.next = cur
+        leftPrev.next = prev
+
+        return dummy.next

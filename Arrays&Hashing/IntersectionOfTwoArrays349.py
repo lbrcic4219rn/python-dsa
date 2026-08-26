077a69cddd2784e04569d40f763820f92525049e
lukabrcic <lukabrcic.lb@gmail.com>
2026-08-26 22:10:50 +0200

    Solution for: IntersectionOfTwoArrays349.py

---
+++ Arrays&Hashing/IntersectionOfTwoArrays349.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        seen = set(nums1)
+
+        res = []
+        for num in nums2:
+            if num in seen:
+                res.append(num)
+                seen.remove(num)
+
+        return res

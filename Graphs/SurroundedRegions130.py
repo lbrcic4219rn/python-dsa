b2a33415e9518ce29d0ac7a34ad9772294850901
lukabrcic <lukabrcic.lb@gmail.com>
2026-09-21 13:50:18 +0200

    Reformat: SurroundedRegions130

--- Graphs/SurroundedRegions130.py
+++ Graphs/SurroundedRegions130.py
@@ -1,6 +1,7 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
+
         def dfs(r, c):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != "O":
                 return
